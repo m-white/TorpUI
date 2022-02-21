59843afc48ad9746fd346dfae7d9b02ac60802f7
m-white <whitejmatt@gmail.com>
2022-02-20 19:22:20 -0500

    switch details instance to use

--- TorpUI/AddOns/Details.lua
+++ TorpUI/AddOns/Details.lua
@@ -3,10 +3,10 @@ local _, TorpUi = ...
 local T_Details = TorpUi.AddOn:new{ profileString = TorpUi.PROFILE_STRINGS.DETAILS }
 
 function T_Details:Apply()
-  local detailsProfile = _detalhes:GetProfile(TorpUi.PROFILE_NAME, false)
+  local detailsProfile = Details:GetProfile(TorpUi.PROFILE_NAME, false)
 
   if (detailsProfile) then
-    _detalhes:EraseProfile(TorpUi.PROFILE_NAME)
+    Details:EraseProfile(TorpUi.PROFILE_NAME)
   end
 
   Details:ImportProfile(self.profileString, TorpUi.PROFILE_NAME)
